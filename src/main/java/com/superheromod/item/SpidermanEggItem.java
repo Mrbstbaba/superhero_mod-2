d4aa614d6048faea2f3dfc58977b7775df485f52
Mrbstbaba <inallardan@gmail.com>
2026-09-23 18:52:52 +0300

    Update SpidermanEggItem.java

--- src/main/java/com/superheromod/item/SpidermanEggItem.java
+++ src/main/java/com/superheromod/item/SpidermanEggItem.java
@@ -10,6 +10,7 @@ import net.minecraft.util.Formatting;
 import net.minecraft.world.World;
 
 import java.util.List;
+import net.minecraft.client.item.TooltipContext;
 
 public class SpidermanEggItem extends Item {
     public SpidermanEggItem(Settings settings) {
