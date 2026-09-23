f30228aded380b9c3014713918e73804ca98a44a
Mrbstbaba <inallardan@gmail.com>
2026-09-23 18:46:01 +0300

    Update HulkItem.java

--- src/main/java/com/superheromod/item/HulkItem.java
+++ src/main/java/com/superheromod/item/HulkItem.java
@@ -10,7 +10,6 @@ import net.minecraft.util.Formatting;
 import net.minecraft.world.World;
 
 import java.util.List;
-import net.minecraft.client.item.TooltipContext;
 
 public class HulkItem extends Item {
     public HulkItem(Settings settings) {
